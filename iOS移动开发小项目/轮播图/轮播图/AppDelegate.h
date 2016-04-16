34ad447a3d6ec79de210a6a96eaaf4574def3958
MR.K <root@mk>
2016-04-16 11:36:41 +0800

    实现自动轮播、页面控制器(小圆点)跟随

---
+++ iOS移动开发小项目/轮播图/轮播图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  轮播图
+//
+//  Created by lanou on 16/4/13.
+//  Copyright © 2016年 MR.K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS移动开发小项目/轮播图/轮播图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  轮播图
+//
+//  Created by lanou on 16/4/13.
+//  Copyright © 2016年 MR.K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
